27e1440396dae994525f1a417390b407b7044c23
Anton Borisov <borisov2011@gmail.com>
2023-08-30 12:12:59 +0300

    добавлена возможность редактирования записи

--- src/TrainingApp.tsx
+++ src/TrainingApp.tsx
@@ -8,44 +8,82 @@ interface Training {
 
 const TrainingApp: React.FC = () => {
   const [trainings, setTrainings] = useState<Training[]>([]);
+  const [editingTraining, setEditingTraining] = useState<Training | null>(null);
+
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.currentTarget;
     const date = form.date.value;
     const distance = parseFloat(form.distance.value);
-
-    const existingTrainingIndex = trainings.findIndex(training => training.date === date);
-
-    if (existingTrainingIndex !== -1) {
-      trainings[existingTrainingIndex].distance += distance;
-      setTrainings([...trainings]);
+  
+    if (editingTraining) {
+      const updatedTrainings = trainings.map(training => {
+        if (training.date === editingTraining.date) {
+          return { ...training, distance};
+        }
+        return training;
+      });
+      setTrainings(updatedTrainings);
+      setEditingTraining(null);
     } else {
-      setTrainings([{ date, distance }, ...trainings].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()));
+      const existingTrainingIndex = trainings.findIndex(training => training.date === date);
+      if (existingTrainingIndex !== -1) {
+        trainings[existingTrainingIndex].distance += distance;
+        setTrainings([...trainings]);
+      } else {
+        setTrainings([{ date, distance }, ...trainings].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()));
+      }
     }
-
+  
     form.reset();
   };
+  
 
   const handleDelete = (date: string) => {
     const updatedTrainings = trainings.filter(training => training.date !== date);
     setTrainings(updatedTrainings);
+    setEditingTraining(null);
   };
+
+  const startEditing = (date: string) => {
+    const trainingToEdit = trainings.find(training => training.date === date);
+    if (trainingToEdit) {
+      setEditingTraining(trainingToEdit);
+    }
+  };
+  
+  
 
   return (
     <div className="App">
       <h1>Учет тренировок</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Дата:
-          <input type="date" name="date" required />
-        </label>
-        <label>
-          Пройдено км:
-          <input type="number" name="distance" required />
-        </label>
-        <button type="submit">Добавить</button>
-      </form>
+      {editingTraining ? (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Дата:
+        <input type="date" name="date" required defaultValue={editingTraining.date} />
+      </label>
+      <label>
+        Пройдено км:
+        <input type="number" name="distance" required />
+      </label>
+      <button type="submit">Изменить</button>
+      <button onClick={() => setEditingTraining(null)}>Отмена</button>
+    </form>
+  ) : (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Дата:
+        <input type="date" name="date" required />
+      </label>
+      <label>
+        Пройдено км:
+        <input type="number" name="distance" required />
+      </label>
+      <button type="submit">Добавить</button>
+    </form>
+  )}
       <table>
         <thead>
           <tr>
@@ -60,13 +98,17 @@ const TrainingApp: React.FC = () => {
               <td>{training.date}</td>
               <td>{training.distance.toFixed(1)}</td>
               <td>
+              <td className='button-container'>
+                <button onClick={() => startEditing(training.date)}>✎</button>
                 <button onClick={() => handleDelete(training.date)}>✘</button>
+              </td>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
+    
   );
 }
 
